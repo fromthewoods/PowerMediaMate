15bcab91921e19ac599c5dc3978bd883ce7d3901
fromthewoods <jbaltrus@gmail.com>
2016-03-03 21:45:00 -0700

    Adding Get-DP and Test.

--- PowerMediaMate.Tests.ps1
+++ PowerMediaMate.Tests.ps1
@@ -32,64 +32,127 @@ $expected += (New-Object PSObject -Property $prop)
 
 #endregion
 
-
-
-Describe -Tags "Module" "Module: $moduleName.psm1" {
-
+Describe -Tags 'DP' 'Get-SevenZip' {
     InModuleScope -ModuleName $moduleName {
-
-# The location where uTorrent lives.
-$uTorrent = "TestDrive:\"
-
-$prop = [ordered] @{
-    Base = $uTorrent
-    
-    uTorrentDL      = $uTorrent + "Downloads"          # The location where uTorrent downloads files to.
-    tempExtractPath = $uTorrent + "ExtractedDownloads" # The directory where you want the files to be temporarily extracted to. 
-    logLocation     = $uTorrent + "Logs"               # Where the PMM log file will be to written to.
-    sfvLogsDir      = $uTorrent + "SfvLogs"            # Where the sfv evaulations will be logged to
-    MoviesDir       = $uTorrent + "Movies"             # Directories where the Renamers output files
-    TVEpsDir        = $uTorrent + "TV"                 # Directories where the Renamers output files
-    
-    videofiletypes = @('*.avi','*.mkv','*.m2ts','*.mpg','*.mpeg','*.mp4') # Files types to include when resorting to copying files
-    
-    # DEPENDENCIES
-    SevenZip = "$((.\Get-InstalledApp.ps1 -AppID '7-Zip').InstallLocation)7z.exe"       # Placeholder for the 7z.exe location
-    Quicksfv = (Get-ItemProperty HKLM:\SOFTWARE\Classes\File_Verification_Database\DefaultIcon).'(default)'.split(',')[0] # Placeholder for the QuickSFV.exe location
-}
-$uTorrent = New-Object PSObject  -Property $prop
-
         # Overwrite Write-Log while in testing
         Mock -CommandName Write-Log -MockWith { Write-Host $args[1] }
-        
-        #It "Get-SFV: no dir" {
-        #    Get-SFV -Directory $uTorrent.uTorrentDL | Should Throw
-        #} Need to work on throwing and error handling
-        
-        It "Get-SFV: 0 sfv in dir" {
-            New-Item -Path $uTorrent.uTorrentDL -ItemType Directory | Out-Null
-            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
-            $actual.Count  | Should Be 1
-            $actual.GetType().Name | Should Be 'DirectoryInfo'
+		#Mock -CommandName Write-Log -MockWith {} 
+
+        Mock -CommandName Get-InstalledApp -MockWith { New-Object -TypeName psobject -Property @{AppID = '7-Zip'; InstallLocation = 'D:\derp\' } }
+        It 'Should return a fake location' {
+            Get-SevenZip | Should Be 'D:\derp\7z.exe'
         }
 
-        It "Get-SFV: 1 sfv in dir." {
-            New-Item -Path $($uTorrent.uTorrentDL + "\test.sfv")
-            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
-            $actual.Count  | Should Be 1
-            $actual.GetType().Name | Should Be 'FileInfo'
+        Mock -CommandName Get-InstalledApp -MockWith { Return $null }
+        Mock -CommandName Get-Item -MockWith { Return $true }
+        Mock -CommandName Get-ItemProperty -MockWith { New-Object -TypeName psobject -Property @{ Path = 'D:\derp\'} }
+        It 'Should return a fake location' {
+            Get-SevenZip | Should Be 'D:\derp\7z.exe'
         }
 
-        It "Get-SFV: 2 sfv's in sub-dirs." {
-            mkdir "$($uTorrent.uTorrentDL)\test"
-            New-Item -Path $($uTorrent.uTorrentDL + "\test\test2.sfv")
-            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
-            $actual.Count  | Should Be 2
-            $actual.GetType().Name | Should Be 'Object[]'
+        Mock -CommandName Get-InstalledApp -MockWith { Return $null }
+        Mock -CommandName Get-Item -MockWith { Return $null }
+        It 'Should return false.' {
+            Get-SevenZip | Should Be $false
         }
     }
-}
+} 
 
+#Describe -Tags "DP" "Get-Dependencies" {
+#    InModuleScope -ModuleName $moduleName {
+#        # Overwrite Write-Log while in testing
+#        Mock -CommandName Write-Log -MockWith { Write-Host $args[1] }
+#		#Mock -CommandName Write-Log -MockWith {} 
+#
+#        
+#        Mock -CommandName Get-InstalledApp.ps1 -MockWith { Return $null }
+#        
+#        It "Should return null" {
+#            Test-Domain -Name "SHRHST" | Should Be $true
+#        }
+#
+#        It "The domain should NOT match." {
+#            Test-Domain -Name "fail" | Should Be $false
+#        }
+#    }
+#} 
+
+
+
+#Describe -Tags "Module" "Module: $moduleName.psm1" {
+#
+#    InModuleScope -ModuleName $moduleName {
+#
+## The location where uTorrent lives.
+#$uTorrent = "TestDrive:\"
+#
+#$prop = [ordered] @{
+#    Base = $uTorrent
+#    
+#    uTorrentDL      = $uTorrent + "Downloads"          # The location where uTorrent downloads files to.
+#    tempExtractPath = $uTorrent + "ExtractedDownloads" # The directory where you want the files to be temporarily extracted to. 
+#    logLocation     = $uTorrent + "Logs"               # Where the PMM log file will be to written to.
+#    sfvLogsDir      = $uTorrent + "SfvLogs"            # Where the sfv evaulations will be logged to
+#    MoviesDir       = $uTorrent + "Movies"             # Directories where the Renamers output files
+#    TVEpsDir        = $uTorrent + "TV"                 # Directories where the Renamers output files
+#    
+#    videofiletypes = @('*.avi','*.mkv','*.m2ts','*.mpg','*.mpeg','*.mp4') # Files types to include when resorting to copying files
+#    
+#    # DEPENDENCIES
+#    SevenZip = "$((.\Get-InstalledApp.ps1 -AppID '7-Zip').InstallLocation)7z.exe"       # Placeholder for the 7z.exe location
+#    Quicksfv = (Get-ItemProperty HKLM:\SOFTWARE\Classes\File_Verification_Database\DefaultIcon).'(default)'.split(',')[0] # Placeholder for the QuickSFV.exe location
+#}
+#$uTorrent = New-Object PSObject  -Property $prop
+#
+#        # Overwrite Write-Log while in testing
+#        Mock -CommandName Write-Log -MockWith { Write-Host $args[1] }
+#        
+#        #It "Get-SFV: no dir" {
+#        #    Get-SFV -Directory $uTorrent.uTorrentDL | Should Throw
+#        #} Need to work on throwing and error handling
+#        
+#        It "Get-SFV: 0 sfv in dir" {
+#            New-Item -Path $uTorrent.uTorrentDL -ItemType Directory | Out-Null
+#            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
+#            $actual.Count  | Should Be 1
+#            $actual.GetType().Name | Should Be 'DirectoryInfo'
+#        }
+#
+#        It "Get-SFV: 1 sfv in dir." {
+#            New-Item -Path $($uTorrent.uTorrentDL + "\test.sfv")
+#            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
+#            $actual.Count  | Should Be 1
+#            $actual.GetType().Name | Should Be 'FileInfo'
+#        }
+#
+#        It "Get-SFV: 2 sfv's in sub-dirs." {
+#            mkdir "$($uTorrent.uTorrentDL)\test"
+#            New-Item -Path $($uTorrent.uTorrentDL + "\test\test2.sfv")
+#            $actual = Get-SFV -Directory $uTorrent.uTorrentDL
+#            $actual.Count  | Should Be 2
+#            $actual.GetType().Name | Should Be 'Object[]'
+#        }
+#    }
+#}
+#
+#Describe -Tags "Module" "Module: $moduleName.psm1" {
+#
+#    InModuleScope -ModuleName $moduleName {
+#        # Overwrite Write-Log while in testing
+#        Mock -CommandName Write-Log -MockWith { Write-Host $args[1] }
+#		#Mock -CommandName Write-Log -MockWith {} 
+#
+#        New-Item -Path $uTorrent.sfvLogsDir -ItemType Directory | Out-Null
+#        
+#        It "The domain should match." {
+#            Test-Domain -Name "SHRHST" | Should Be $true
+#        }
+#
+#        It "The domain should NOT match." {
+#            Test-Domain -Name "fail" | Should Be $false
+#        }
+#    }
+#} 
 
 #Describe -Tags "Script" "Script: _Start.ps1" {
 #
--- PowerMediaMate.psm1
+++ PowerMediaMate.psm1
@@ -1,4 +1,240 @@
-﻿function Get-Dependencies {
+﻿function Get-InstalledApp {
+<#
+.Synopsis
+   Outputs installed applications on one or more computers that match one or more criteria.
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Get-InstalledApp -AppID '7-Zip'
+.EXAMPLE
+   Get-InstalledApp -AppName '*7-Zip*'
+.LINK
+   http://windowsitpro.com/powershell/what-applications-are-installed-computers-your-network
+#>
+# Written by Bill Stewart (bstewart@iname.com)
+#
+# Outputs installed applications on one or more computers that match one or
+# more criteria.
+
+    param([String[]] $ComputerName,
+          [String] $AppID,
+          [String] $AppName,
+          [String] $Publisher,
+          [String] $Version,
+          [Switch] $MatchAll,
+          [Switch] $Help
+         )
+    
+    $HKLM = [UInt32] "0x80000002"
+    $UNINSTALL_KEY = "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+    
+    # Outputs a usage message and ends the script.
+    function usage {
+      $scriptname = $SCRIPT:MYINVOCATION.MyCommand.Name
+    
+      "NAME"
+      "    $scriptname"
+      ""
+      "SYNOPSIS"
+      "    Outputs installed applications on one or more computers that match one or"
+      "    more criteria."
+      ""
+      "SYNTAX"
+      "    $scriptname [-computername <String[]>] [-appID <String>]"
+      "    [-appname <String>] [-publisher <String>] [-version <String>] [-matchall]"
+      ""
+      "PARAMETERS"
+      "    -computername <String[]>"
+      "        Outputs applications on the named computer(s). If you omit this"
+      "        parameter, the local computer is assumed."
+      ""
+      "    -appID <String>"
+      "        Select applications with the specified application ID. An application's"
+      "        appID is equivalent to its registry subkey in the location"
+      "        HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall. For Windows"
+      "        Installer-based applications, this is the application's product code"
+      "        GUID (e.g. {3248F0A8-6813-11D6-A77B-00B0D0160060})."
+      ""
+      "    -appname <String>"
+      "        Select applications with the specified application name. The appname is"
+      "        the application's name as it appears in the Add/Remove Programs list."
+      ""
+      "    -publisher <String>"
+      "        Select applications with the specified publisher name."
+      ""
+      "    -version <String>"
+      "        Select applications with the specified version."
+      ""
+      "    -matchall"
+      "        Output all matching applications instead of stopping after the first"
+      "        match."
+      ""
+      "NOTES"
+      "    All installed applications are output if you omit -appID, -appname,"
+      "    -publisher, and -version. Also, the -appID, -appname, -publisher, and"
+      "    -version parameters all accept wildcards (e.g., -version 5.2.*)."
+    
+      exit
+    }
+    
+    function main {
+      # If -help is present, output the usage message.
+      if ($Help) {
+        usage
+      }
+    
+      # Create a hash table containing the requested application properties.
+      #CALLOUT A
+      $propertyList = @{}
+      if ($AppID -ne "")     { $propertyList.AppID = $AppID }
+      if ($AppName -ne "")   { $propertyList.AppName = $AppName }
+      if ($Publisher -ne "") { $propertyList.Publisher = $Publisher }
+      if ($Version -ne "")   { $propertyList.Version = $Version }
+      #END CALLOUT A
+    
+      # Use the local computer's name if no computer name(s) specified.
+      if ($ComputerName -eq $NULL) {
+        $ComputerName = $ENV:COMPUTERNAME
+      }
+    
+      # Iterate the computer name(s).
+      foreach ($machine in $ComputerName) {
+        $err = $NULL
+    
+        # If WMI throws a RuntimeException exception,
+        # save the error and continue to the next statement.
+        #CALLOUT B
+        trap [System.Management.Automation.RuntimeException] {
+          set-variable err $ERROR[0] -scope 1
+          continue
+        }
+        #END CALLOUT B
+    
+        # Connect to the StdRegProv class on the computer.
+        #CALLOUT C
+        $regProv = [WMIClass] "\\$machine\root\default:StdRegProv"
+    
+        # In case of an exception, write the error
+        # record and continue to the next computer.
+        if ($err) {
+          write-error -errorrecord $err
+          continue
+        }
+        #END CALLOUT C
+    
+        # Enumerate the Uninstall subkey.
+        $subkeys = $regProv.EnumKey($HKLM, $UNINSTALL_KEY).sNames
+        foreach ($subkey in $subkeys) {
+          # Get the application's display name.
+          $name = $regProv.GetStringValue($HKLM,
+            (join-path $UNINSTALL_KEY $subkey), "DisplayName").sValue
+          # Only continue of the application's display name isn't empty.
+          if ($name -ne $NULL) {
+            # Create an object representing the installed application.
+            $output = new-object PSObject
+            $output | add-member NoteProperty ComputerName -value $machine
+            $output | add-member NoteProperty AppID -value $subkey
+            $output | add-member NoteProperty AppName -value $name
+            $output | add-member NoteProperty Publisher -value `
+              $regProv.GetStringValue($HKLM,
+              (join-path $UNINSTALL_KEY $subkey), "Publisher").sValue
+            $output | add-member NoteProperty Version -value `
+              $regProv.GetStringValue($HKLM,
+              (join-path $UNINSTALL_KEY $subkey), "DisplayVersion").sValue
+    
+            $output | add-member NoteProperty InstallLocation -value `
+              $regProv.GetStringValue($HKLM,
+              (join-path $UNINSTALL_KEY $subkey), "InstallLocation").sValue
+    
+            $output | add-member NoteProperty UninstallString -value `
+              $regProv.GetStringValue($HKLM,
+              (join-path $UNINSTALL_KEY $subkey), "UninstallString").sValue
+            # If the property list is empty, output the object;
+            # otherwise, try to match all named properties.
+            if ($propertyList.Keys.Count -eq 0) {
+              $output
+            } else {
+              #CALLOUT D
+              $matches = 0
+              foreach ($key in $propertyList.Keys) {
+                if ($output.$key -like $propertyList.$key) {
+                  $matches += 1
+                }
+              }
+              # If all properties matched, output the object.
+              if ($matches -eq $propertyList.Keys.Count) {
+                $output
+                # If -matchall is missing, break out of the foreach loop.
+                if (-not $MatchAll) {
+                  break
+                }
+              }
+              #END CALLOUT D
+            }
+          }
+        }
+      }
+    }
+    
+    main
+}
+
+function Get-SevenZip {
+<#
+.Synopsis
+   Returns the location of 7z.exe, if detected. Otherwise it returns false.
+#>
+
+    Try
+    {
+        #. .\Get-InstalledApp.ps1
+    	$SevenZip = Get-InstalledApp -AppName '*7-Zip*'
+        If ($SevenZip.InstallLocation)
+        {
+            $SevenZip = $SevenZip.InstallLocation + '7z.exe'
+        }
+        ElseIf (Get-Item -Path HKLM:\SOFTWARE\7-Zip)
+        {
+            $SevenZip = (Get-ItemProperty -Path HKLM:\SOFTWARE\7-Zip -Name Path).Path + '7z.exe'
+        }
+        Else { $SevenZip = $false }
+
+        Return $SevenZip
+    }
+    Catch
+    {
+        Write-Log "ERROR: $($_.Exception.Message)"
+        Write-Log "ERROR: $($_.InvocationInfo.PositionMessage.Split('+')[0])"
+        Exit 1
+    } 
+}
+
+function Get-QuickSFV {
+<#
+.Synopsis
+   Returns the location of QuickSfv.exe, if detected. Otherwise it returns false.
+#>
+
+    Try
+    {
+        $regkey = Get-ItemProperty -Path HKLM:\SOFTWARE\Classes\File_Verification_Database\DefaultIcon -ErrorAction SilentlyContinue
+        If ($regkey)
+        {
+            $Quicksfv = $regkey.'(default)'.split(',')[0]
+        } 
+        Else { $Quicksfv = $false }
+
+        Return $Quicksfv
+    }
+    Catch
+    {
+        Write-Log "ERROR: $($_.Exception.Message)"
+        Write-Log "ERROR: $($_.InvocationInfo.PositionMessage.Split('+')[0])"
+        Exit 1
+    }
+}
+
+function Get-Dependencies {
 <#
 .Synopsis
    Short description
@@ -13,18 +249,8 @@
         $allDependenciesArePresent = $true
 
         #region ~~~~~ Detect 7z.exe ~~~~~~~~~~~~~~~~~~~~~~~
-        $SevenZip = .\Get-InstalledApp.ps1 | Where-Object { $_.AppName -like '*7-Zip*'}
-        If ($SevenZip.InstallLocation)
-        {
-            $SevenZip = $SevenZip.InstallLocation + '7z.exe'
-        }
-        ElseIf (Get-Item -Path HKLM:\SOFTWARE\7-Zip)
-        {
-            $SevenZip = (Get-ItemProperty -Path HKLM:\SOFTWARE\7-Zip -Name Path).Path + '7z.exe'
-        }
-        Else { $SevenZip = $false }
-
-        If ($SevenZip -eq $null)
+        $SevenZip = Get-SevenZip
+        If ($SevenZip -eq $false)
         {
             $allDependenciesArePresent = $false
         }
@@ -35,17 +261,7 @@
         #endregion ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         #region ~~~~~ Detect QuickSFV.exe ~~~~~~~~~~~~~~~~~
-        #$Quicksfv = Invoke-Command -ScriptBlock { $regkey = Get-ItemProperty HKLM:\SOFTWARE\Classes\File_Verification_Database\DefaultIcon -ErrorAction SilentlyContinue
-        #                                          If ($regkey) { $regkey.'(default)'.split(',')[0] } 
-        #                                          Else {$false }
-        #                                          }
-        $regkey = Get-ItemProperty HKLM:\SOFTWARE\Classes\File_Verification_Database\DefaultIcon -ErrorAction SilentlyContinue
-        If ($regkey)
-        {
-            $Quicksfv = $regkey.'(default)'.split(',')[0]
-        } 
-        Else { $Quicksfv = $false }
-
+        $Quicksfv = Get-QuickSFV
         If (!$Quicksfv)
         {
             $allDependenciesArePresent = $false
@@ -145,6 +361,11 @@ function Test-SfvLog {
         [ValidateScript({($_.GetType().Name -eq 'FileInfo') -or
                          ($_.GetType().Name -eq 'DirectoryInfo')})]
         $item
+    ,
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$false,
+                   Position=1)]
+        [string]$sfvLogsDir
     )
 
     Process
